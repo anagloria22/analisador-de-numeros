15ee18e594ae1d112aab6be9f6e285e7b5ee8c01
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-04-09 21:53:40 -0300

    update

--- analisar.js
+++ analisar.js
@@ -1,12 +1,15 @@
-function adicionar () {
-    let num = document.getElementById('inumero')
-    let analise = document.getElementById('ianalisador')
+let num = document.getElementById('inumero')
+let analise = document.getElementById('ianalisador')
+let resposta = document.getElementById('resposta')
+let valores = []
 
+function adicionar () {
     if (num.value.length <= 0 || num.value.length > 100 || num.value.length == num) {
         alert('Valor inválido ou já encontrado na lista')
     } else {
         let n = Number(num.value)
         if (n <= 100) {
+            valores.push(Number(num.value))
             var item = document.createElement('option')
             item.text = `Valor ${n} adicionado.`
             analise.appendChild(item)
@@ -15,3 +18,13 @@ function adicionar () {
     }
 }
 
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        
+    }
+}
