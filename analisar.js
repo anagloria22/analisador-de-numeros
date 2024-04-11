c5dabd7bed7b476c3d822713a5636a586c7cae33
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-04-10 22:14:52 -0300

    Update analisar.js

--- analisar.js
+++ analisar.js
@@ -2,6 +2,7 @@ let num = document.getElementById('inumero')
 let analise = document.getElementById('ianalisador')
 let resposta = document.getElementById('resposta')
 let valores = []
+let soma = 0
 
 function adicionar () {
     if (num.value.length <= 0 || num.value.length > 100 || num.value.length == num) {
@@ -25,6 +26,25 @@ function finalizar() {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores[0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        for (let i = 0; i < valores.length; i++) {
+            soma += Number(valores[i])
+        }
+
+        let media = soma / valores.length
+        
+        resposta.innerHTML = ''
+        resposta.innerHTML += `Ao todo temos ${tot} números cadastrados <br>`
+        resposta.innerHTML += `O número ${maior} é o maior cadastrado <br>`
+        resposta.innerHTML += `O número ${menor} é o menor cadastrado <br>`
+        resposta.innerHTML += `A soma total dos números cadastrados é ${soma} <br>`
+        resposta.innerHTML += `A média dos números cadastrados é ${media}`
         
     }
 }
